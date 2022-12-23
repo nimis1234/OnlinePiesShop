d329367e0542fcb52731321e1e36715ce1a38a38
Nimisha Jacob <nimis1234@gmail.com>
2022-12-22 19:48:20 -0600

    develop pie test

--- OnlineShop/Model/Pie.cs
+++ OnlineShop/Model/Pie.cs
@@ -14,7 +14,8 @@
         public bool InStock { get; set; }
   
         ------------
-       
+       -----------------
+
         public int CategoryId { get; set; }
         public Category Category { get; set; }
         public string Notes { get; set; }
