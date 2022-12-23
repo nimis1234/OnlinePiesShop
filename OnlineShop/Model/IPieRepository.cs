43bef05bebab79dd35d3f4f769f90ff1bd460fab
Nimisha Jacob <nimis1234@gmail.com>
2022-12-22 19:58:30 -0600

    1 commit

--- OnlineShop/Model/IPieRepository.cs
+++ OnlineShop/Model/IPieRepository.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace OnlineShop.Model
 {
@@ -10,6 +7,6 @@ namespace OnlineShop.Model
         IEnumerable<Pie> AllPies { get; }
         IEnumerable<Pie> PiesOfTheWeek { get; }
         Pie GetPieById(int pieId);
-        //IEnumerable<Pie> GetAllPieBasedOnCategories { get; }
+        
     }
 }
